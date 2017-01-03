3a026e16609c1a76abffdfb3186b980c91c762bc
Supasate Choochaisri <supasate.c@gmail.com>
2017-01-03 14:45:57 +0700

    Setup firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import firebase from 'firebase'
 import { Header } from './components/common'
+import config from '../config'
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp(config.firebase)
+  }
+
   render() {
     return (
       <View>
