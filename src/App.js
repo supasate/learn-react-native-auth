f87572b928ea02e1f8e2e0dbf43ae996d50939f6
Supasate Choochaisri <supasate.c@gmail.com>
2017-01-04 12:06:11 +0700

    Display button based on auth condition

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import firebase from 'firebase'
-import { Header } from './components/common'
+import { Header, Button, Spinner, CardSection } from './components/common'
 import LoginForm from './components/LoginForm'
 import config from '../config'
 
 class App extends Component {
+  state = { loggedIn: null }
+
   componentWillMount() {
     firebase.initializeApp(config.firebase)
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true })
+      } else {
+        this.setState({ loggedIn: false })
+      }
+    })
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return <Button>Log Out</Button>
+      case false:
+        return <LoginForm />
+      default:
+        return <Spinner size="large" />
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        <CardSection>
+          {this.renderContent()}
+        </CardSection>
       </View>
     )
   }
